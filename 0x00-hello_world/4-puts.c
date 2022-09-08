d1d592bbcc6059a103d0e7ddf1f6da6970f7a8f0
ukamakaanthony <ukamaka.anthony01@gmail.com>
2022-09-08 10:47:43 -0700

    fingers crossed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-/*
- * main-main block
- * Return:0
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
