2812d639fc6f6de517076d3906fdee38315831ae
ukamakaanthony <ukamaka.anthony01@gmail.com>
2022-09-13 10:55:01 -0700

    updated

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -13,7 +14,7 @@ int main(void)
 	_putchar('c');
 	_putchar('h');
 	_putchar('a');
-	_putchar('r')
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
