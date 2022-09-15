6eec62b57a16e3c698dce75ce9953e9228d0aff5
ukamakaanthony <ukamaka.anthony01@gmail.com>
2022-09-15 12:22:35 -0700

    commiting debugged files

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,4 +1,24 @@
 #include <stdio.h>
 
 /**
- * 
+ * main - cause an infinite loop
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	printf("infinite loop :(\n");
+
+	i = 0;
+
+	/*while (i < 10)*/
+	/*{*/
+	/*      putchar(i);*/
+	/*}*/
+
+	printf("infinite loop avoided! \\o/\n");
+
+	return (0);
+}
