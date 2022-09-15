7af3a3ac5b172255bc9d7641ca570a3d2a4ea6b1
ukamakaanthony <ukamaka.anthony01@gmail.com>
2022-09-15 16:56:35 -0700

    done

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,7 +3,7 @@
 /**
  * main: checks for a digit 0 - 9
  * @c: the number to be checked
- * Return: 1 if c is digit, other wise 0
+ * Return: 1 if c is digit, other wise 0 thats it
  */
 int _isdigit(int c);
 {
