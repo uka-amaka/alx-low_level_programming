4410eb0d289061fc3777efafad1a9c9b57eca6fb
ukamakaanthony <ukamaka.anthony01@gmail.com>
2022-09-19 13:48:46 -0700

    first try

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes a pointer toan int
+ * @n: updates value to 9
+ *
+ * Return: always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+readme file for pointers and array will be updated as soon as am less busy
