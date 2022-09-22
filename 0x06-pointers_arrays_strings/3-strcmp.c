c007d0cb558d629c105054a731efe75d7e5b5d35
ukamakaanthony <ukamaka.anthony01@gmail.com>
2022-09-22 16:06:05 -0700

    hot fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,7 +5,7 @@
  * @s1: string 1 to compare
  * @s2: string 2 to compare
  *
- * Return: less than 0 if string 1 is less than s2, 0 if they are 
+ * Return: less than 0 if string 1 is less than s2, 0 if they are
  * equal more than 0 if s1 is greater than s2
  */
 int _strcmp(char *s1, char *s2)
