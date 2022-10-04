62d73248cc2fbf926aec7efd907d736a0e4d2cac
ukamakaanthony <ukamaka.anthony01@gmail.com>
2022-10-03 18:06:17 -0700

    fixed

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
+ * create_array - prints buffer in hexa
+ * @c: the address of memory to print
  * @size: the size of the memory to print
  *
  * Return: pointer of an array
