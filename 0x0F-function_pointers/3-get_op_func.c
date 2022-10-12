00eb047d4ea81f795b874205d975d57b0d3c9c0f
ukamakaanthony <ukamaka.anthony01@gmail.com>
2022-10-12 15:13:47 -0700

    testing...

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -8,13 +8,13 @@
  * @s: ...
  * Return: ..
  */
-int (*get_op_func(char *s) (int, int))
+int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
 	{ "+", op_add },
 	{ "-", op_sub },
 	{ "*", op_mul },
-	{ "/", op_div }
+	{ "/", op_div },
 	{ "%", op_mod },
 	{ NULL, NULL }
 	};
