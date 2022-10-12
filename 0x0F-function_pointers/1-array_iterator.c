e6693cfa33b5e8aab4a65666fffcd284a05ec957
ukamakaanthony <ukamaka.anthony01@gmail.com>
2022-10-12 14:39:35 -0700

    fixed the bug

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,31 +6,6 @@
  * @action: pointer to function
  * Return: void
  */
-void array_iterator(int *array, size, void (*action)(int))
-{
-	int i;
-
-	if (array == NULL || action == NULL)
-		return;
-
-	for (i = 0; I < (int) size; i++)
-	{
-		action(array[i]);
-	}
-}
-/**
- * array_iterator - execute function on each element
- *
- * of an array
- *
- * @array: given array
- *
- * @size: size of the array
- *
- * @action: pointer to function
- *
- * Return: void
- */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i;
