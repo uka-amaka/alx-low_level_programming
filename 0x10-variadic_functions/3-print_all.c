c5e2798271fac900554d5c1e12c4b24a96aa18b2
ukamakaanthony <ukamaka.anthony01@gmail.com>
2022-10-13 16:40:58 -0700

    commited

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "variadic_functions.h"
-
 /**
  * print_all - print char, integer, float and string
  * @format: format
@@ -22,21 +21,18 @@ void print_all(const char * const format, ...)
 			start = 1;
 			printf("%c", va_arg(list, int));
 			break;
-
 			case 'i':
 				switch (start)
 				{ case 1: printf(", "); }
 				start = 1;
 				printf("%i", va_arg(list, int));
 				break;
-
 			case 'f':
 				switch (start)
 				{ case 1: printf(", "); }
 				start = 1;
 				printf("%f", va_arg(list, double));
 				break;
-
 			case's':
 				switch (start)
 				{ case 1: printf(", "); }
@@ -45,7 +41,6 @@ void print_all(const char * const format, ...)
 					if (p)
 				{ printf("%s", p);
 				break; }
-
 				printf("%p", p);
 				break; }
 			j++;
