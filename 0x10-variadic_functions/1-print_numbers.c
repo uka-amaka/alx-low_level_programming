60c810ea9c4b05615d10b62b4c672d2806936bb2
ukamakaanthony <ukamaka.anthony01@gmail.com>
2022-10-13 16:37:02 -0700

    commited

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,6 +13,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ap;
 	unsigned int i;
 
+	va_start(ap, n);
+
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(ap, int));
