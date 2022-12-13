4f0f62991f199faf7ff4b9de51aa48e3acdae7ed
uka-amaka <ukamaka.anthony01@gmail.com>
2022-12-12 17:34:50 -0800

    fixed

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -1,12 +1,6 @@
 #include "main.h"
-#include <stdio.h>
-/**
- * main - check the code
- *
- * Return: Always EXIT_SUCCESS.
- */
 int main(void) 
 {
-  printf("%d\n", _strlen("My Dyn Lib"));  
-  return (EXIT_SUCCESS);
+  _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+  return (0); 
 }
